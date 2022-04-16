3263c2a3c6323d6bab355e6989d1d831b2f8d784
Filippo Ziche <filippo.ziche@gmail.com>
2022-04-16 15:28:18 +0200

    Bootstrap

---
+++ build/CMakeFiles/MINDFIELD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MINDFIELD.dir/source/main.cpp.o"
+  "MINDFIELD"
+  "MINDFIELD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MINDFIELD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+#include <config.hpp>
+
+int main(int argc, char** argv) {
+
+    std::cout << "Versione: " 
+              << MINDFIELD_VERSION_MAJOR 
+              << "." 
+              << MINDFIELD_VERSION_MINOR 
+              << std::endl;
+
+    
+    
+    return 0;
+}
